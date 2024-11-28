0a6c85edc6e7ee76a25d04a44a26b2afbac9ef44
TamHM1410 <hunhminhtam@gmail.com>
2024-11-28 09:29:18 +0700

    update product

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,12 +1,7 @@
 // sections
-'use client'
 import { ModernNewPasswordView ,ModernNewVerifyEmail} from 'src/sections/auth-demo/modern';
 
 // ----------------------------------------------------------------------
-
-export const metadata = {
-  title: 'Auth Modern: Verify',
-};
 
 export default function ModernVerifyPage() {
   return <ModernNewVerifyEmail />;
