8ddff727ff5f260954213d342b0fd8f4fb536222
TamHM1410 <hunhminhtam@gmail.com>
2024-12-26 08:31:06 +0700

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,5 @@ export default withAuth({
   });
 
   export const config = {
-    matcher: ["/dashboard/booking-service","/dashboard","/dashboard/analytics"
-      ,"/logout","/dashboard/user/account","/pricing","/pricing/checkout",
-      "/dashboard/booking-list","/dashboard/hold-residences-list","/dashboard/manage-customer",
-    "/dashboard/report-list","/dashboard/chat","/dashboard/user/account/","/dashboard/yourpackage","/dashboard/housekeepers"], // Protect the /protected route
+    matcher: ["/dashboard(.*)"]
   };
